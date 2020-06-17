7e99511ac8380d13b19882dd4c1fb92a61500f52
Marco Ordoñez <ordonez.marcos21@gmail.com>
2020-06-17 00:41:14 -0500

    Agregamos los estilos de bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//require jquery
+//require popper
+//require bootstrap
+//require sb-admin-2.min
