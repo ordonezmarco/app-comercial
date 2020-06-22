db9ff581a9249bb038c442a170cc8d1cabb35dfc
Marco Ordoñez <ordonez.marcos21@gmail.com>
2020-06-21 20:56:47 -0500

    Controlador de categorias

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do  
+  #get 'categories/index'
+  #get 'categories/new'
+  #get 'categories/edit'
+  #get 'categories/create'
+  #get 'categories/update'
+  #get 'categories/destroy'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  resources :categories
+
 end
